0a55a1808af9938bd5caaa948b6fb667be7d1f18
Antoniocalzado02 <71628587+Antoniocalzado02@users.noreply.github.com>
2023-01-10 08:30:36 +0100

    Delete MiTiendaSpringCalzadoAntonio directory

--- MiTiendaSpringCalzadoAntonio/src/main/java/com/jacaranda/MiTienda/repository/MaterialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jacaranda.MiTienda.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jacaranda.MiTienda.model.Material;
-
-public interface MaterialRepository extends JpaRepository<Material, Integer> {
-
-}
